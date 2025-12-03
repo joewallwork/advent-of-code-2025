b53628a2e5098080d72dc6a5aceebf630256519e
Joe Wallwork <jw2423@cam.ac.uk>
2025-12-03 18:09:48 +0000

    Iterate over batteries in day 3

---
+++ day03/src/main.rs
@@ -0,0 +1,41 @@
+use std::fs;
+use substring::Substring;
+
+fn main() {
+    // Load the input file
+    // TODO: Handle both cases with a command line argument
+    let file_path = String::from("test.dat");
+    // let file_path = String::from("input.dat");
+    println!("Reading input file '{file_path}'");
+    let contents = fs::read_to_string(file_path).expect("Failed to read input file");
+
+    // Define solution variables as mutable integers
+    let mut part1: i32 = 0;
+    let mut part2: i32 = 0;
+
+    // Loop over codes in the input file
+    for bank in contents.split("\n") {
+        match bank {
+            "" => {} // Skip any empty strings
+            _ => {
+                println!("{}", bank);
+                let mut i = 0;
+                let length = bank.len();
+                for battery in bank.split("") {
+                    match battery {
+                        "" => {}
+                        _ => {
+                            i = i + 1;  // TODO: Neater enumeration
+                            for other in bank.substring(i, length).chars() {
+                                println!("{} {}", battery, other);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    println!("part 1: {}", part1);
+    println!("part 2: {}", part2)
+}
